7ef395ae284b61c3208178518445208c809326c6
Ramesh Aryal <ramesharyal@socoro.com.au>
2023-02-22 20:04:54 +1100

    facebook authentication is added

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import GithubProvider from "next-auth/providers/github"
+import FacebookProvider from "next-auth/providers/facebook";
 export const authOptions = {
   // Configure one or more authentication providers
   secret: process.env.NEXT_AUTH_SECRET,
@@ -13,6 +14,11 @@ export const authOptions = {
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!
+    })
+
    
     // ...add more providers here
   ],
